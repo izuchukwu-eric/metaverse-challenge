8985531bdf4157abe834c94ec666634a8fd4a26e
Izuchukwu Onukwube <onukwubeizu@gmail.com>
2021-12-29 20:37:15 +0100

    moralis auth done

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,28 @@
 import Image from "next/image";
+import Laptop from "../images/laptop.jpg";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
+
   return (
-    <div className="bg-black relative text-white">
+    <div className="bg-black relative">
       <h1>I am the login screen</h1>
-      <div></div>
+      <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+        <Image
+          className="object-cover rounded-full"
+          src={Laptop}
+          width={200}
+          height={200}
+        />
+
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+        >
+          Login to the METAVERSE
+        </button>
+      </div>
 
       <div className="w-full h-screen">
         <Image
