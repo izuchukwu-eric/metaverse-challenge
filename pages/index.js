a29cdd1e4454bc8988bfb443d75a554ccb58f37b
Izuchukwu Onukwube <onukwubeizu@gmail.com>
2022-01-03 15:31:55 +0100

    Header and Change username component done

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
 
 export default function Home() {
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to the Metaverse</h1>
-      <button onClick={logout}>Logout</button>
+      <div className="max-w-screen-2xl mx-auto">
+        {/**Header */}
+        <Header />
+        {/**Messages */}
+      </div>
     </div>
   );
 }
