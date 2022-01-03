632d99016ede0ff2521ef5b7b609d3679b986d40
Izuchukwu Onukwube <onukwubeizu@gmail.com>
2022-01-03 23:08:32 +0100

    chatting functionality done

--- components/Messages.js
+++ components/Messages.js
@@ -1,8 +1,29 @@
+import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
+import Message from "./Message";
 import SendMessage from "./SendMessage";
+
+//only show messages from the last 15min
+const MINS_DURATION = 15;
 
 function Messages() {
   const { user } = useMoralis();
+  const endOfMessagesRef = useRef(null);
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true, //real time listener
+    }
+  );
+  console.log(data);
 
   return (
     <div className="pb-56">
@@ -13,13 +34,17 @@ function Messages() {
         />
       </div>
 
-      <div>{/**Messages */}</div>
-
-      <div className="flex justify-center">
-        <SendMessage />
+      <div className="space-y-10 p-4">
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
       </div>
 
-      <div className="text-center text-gray-400 mt-5">
+      <div className="flex justify-center">
+        <SendMessage endOfMessagesRef={endOfMessagesRef} />
+      </div>
+
+      <div ref={endOfMessagesRef} className="text-center text-gray-400 mt-5">
         <p>You're up to date {user.getUsername()}</p>
       </div>
     </div>
--- components/SendMessage.js
+++ components/SendMessage.js
@@ -1,16 +1,62 @@
+import { useState } from "react";
 import { useMoralis } from "react-moralis";
 
-function SendMessage() {
-  const { user } = useMoralis();
+function SendMessage({ endOfMessagesRef }) {
+  const { user, Moralis } = useMoralis();
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (!message) return;
+
+    //basically creating a table in the moralis db
+    const Messages = Moralis.Object.extend("Messages");
+
+    //get an instance of the Messages table
+    const messages = new Messages();
+
+    messages
+      .save({
+        messages: message,
+        username: user.getUsername(),
+        ethAddress: user.get("ethAddress"),
+      })
+      .then(
+        (message) => {
+          //The object was saved successfully
+        },
+        (error) => {
+          //The save failed
+
+          console.log(error.message);
+        }
+      );
+
+    //scrolls user down as we type a message
+    endOfMessagesRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    setMessage("");
+  };
 
   return (
-    <form className="flex fixed bottom-10 bg-black opacity-80 px-6 py-4 max-w-2xl shadow-xl rounded-full w-11/12">
+    <form className="flex fixed bottom-10 bg-black opacity-80 px-6 py-4 max-w-2xl shadow-xl rounded-full border-blue-400 border-4 w-11/12">
       <input
         className=" flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5"
         type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
         placeholder={`Enter a Message ${user.getUsername()}...`}
       />
-      <button className="font-bold text-pink-500">Send</button>
+      <button
+        type="submit"
+        onClick={sendMessage}
+        className="font-bold text-pink-500"
+      >
+        Send
+      </button>
     </form>
   );
 }
